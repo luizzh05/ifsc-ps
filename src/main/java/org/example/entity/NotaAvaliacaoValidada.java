a01190dad52c050cea5c4cdbcb4098a4a356a11f
Luiz Henrique <lhmendes0911@gmail.com>
2026-03-19 17:02:56 -0300

    fix: retirando logica de validacao do construtor

--- src/main/java/org/example/entity/NotaAvaliacaoValidada.java
+++ src/main/java/org/example/entity/NotaAvaliacaoValidada.java
@@ -6,14 +6,14 @@ public class NotaAvaliacaoValidada implements Avaliavel {
     private Avaliavel avaliacao;
 
     public NotaAvaliacaoValidada(Avaliavel avaliacao) {
-        if(avaliacao.nota() < 0 || avaliacao.nota() > 10) {
-            throw new IllegalArgumentException("Nota deve ser entre 0 e 10");
-        }
         this.avaliacao = avaliacao;
     }
 
     @Override
     public double nota() {
-        return this.avaliacao.nota();
+        if(avaliacao.nota() < 0 || avaliacao.nota() > 10) {
+            throw new IllegalArgumentException("Nota deve ser entre 0 e 10");
+        }
+        return avaliacao.nota();
     }
 }
